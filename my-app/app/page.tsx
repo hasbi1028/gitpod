f013a1b12988e8e76d7b35fc4f633b25c09fb4ed
hasbi1028 <35333080+hasbi1028@users.noreply.github.com>
2023-08-27 03:27:26 +0000

    Pending changes exported from your codespace

---
+++ my-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h2>
+        homepage
+      </h2>
+    </div>
+  )
+}
---
+++ my-app/app/products/page.tsx
@@ -0,0 +1,40 @@
+import { title } from "process";
+
+type Product = {
+    id: number;
+    title: string;
+    price: number;
+}
+
+async function getProducts() {
+    const res = await fetch('http://localhost:5000/products');
+    return res.json();
+}
+
+export default async function ProductList() {
+  const products : Product[] = await getProducts();
+    return (
+    <div className="py-10 px-10">
+        <table className="table w-full">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {products.map((product, index) =>(
+                    <tr key={product.id}>
+                        <td>{index + 1}</td>
+                        <td>{product.title}</td>
+                        <td>{product.price}</td>
+                        <td> Action </td>
+                    </tr>   
+        ))}
+        </tbody>
+        </table>
+    </div>
+  )
+}
